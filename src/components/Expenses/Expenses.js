c78f109f8b1600070a2e8b4fc4760138a102c351
Nebyat Tibebu <nebyatweb@users.noreply.github.com>
2022-04-15 23:46:52 -0500

    add new expense and new expense form components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,23 @@
+//Expenses component for lists of components
 import Expense from "./Expense";
 import './Expenses.css'
+import Card from "../UI/Card";
 
 const Expenses = (props) => {
     const expenseList = props.expenses.map((expense) => {
         return ( 
-            <div className="expenses">
+            <div>
                 <Expense title={expense.title} amount={expense.amount} date={expense.date} />
             </div>
          );
     });
     
-    return <div>{expenseList}</div>
+    return(
+        <Card className="expenses">
+            {expenseList}
+        </Card>
+
+    ) 
 }
  
 export default Expenses;
